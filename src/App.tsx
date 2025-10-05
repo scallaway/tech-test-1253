6f2269ec8bd6d1a278daf22bb62950f6ff5c77c3
Scott Callaway <github@scottcallaway.co.uk>
2025-10-05 12:59:01 +0100

    Render very basic spreadsheet layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import { useState } from "react";
+import { useState, type ComponentProps } from "react";
 import { AgGridReact } from "ag-grid-react";
+import { themeQuartz, type ColDef } from "ag-grid-community";
+
+const DEFAULT_COLUMN_DEF = {
+	width: 100,
+	resizable: false,
+	sortable: false,
+} satisfies ColDef;
+
+const COLUMNS = [
+	{
+		headerName: "",
+		field: "rowIndex",
+		headerStyle: { width: 50 },
+	},
+	...Array.from({ length: 7 }).map((_, i) => ({
+		field: String.fromCharCode(65 + i).toLocaleUpperCase(),
+	})),
+]; // I think the types for columnDefs are slightly wrong (even the setup
+// tutorial didn't compile properly)
+
+const THEME = themeQuartz.withParams({
+	borderColor: "#000",
+	headerColumnBorder: { style: "solid" },
+	columnBorder: { style: "solid" },
+});
+
+const AUTO_SIZE_STRATEGY = {
+	type: "fitCellContents",
+	skipHeader: true,
+} satisfies ComponentProps<typeof AgGridReact>["autoSizeStrategy"];
 
 function App() {
-	// Row Data: The data to be displayed.
-	const [rowData, setRowData] = useState([
-		{ make: "Tesla", model: "Model Y", price: 64950, electric: true },
-		{ make: "Ford", model: "F-Series", price: 33850, electric: false },
-		{ make: "Toyota", model: "Corolla", price: 29600, electric: false },
-	]);
-
-	// Column Definitions: Defines the columns to be displayed.
-	const [colDefs, setColDefs] = useState([
-		{ field: "make" },
-		{ field: "model" },
-		{ field: "price" },
-		{ field: "electric" },
-	]);
+	const [rowData, setRowData] = useState(() =>
+		Array.from({ length: 10 }).map((_, rowIndex) =>
+			Object.fromEntries(
+				COLUMNS.map((column, columnIndex) => [
+					[column.field],
+					columnIndex === 0 ? rowIndex + 1 : null,
+				]),
+			),
+		),
+	);
 
 	return (
-		<div style={{ height: 500 }}>
-			<AgGridReact rowData={rowData} columnDefs={colDefs} />
+		<div style={{ height: 475, width: 700, overflow: "scroll" }}>
+			<AgGridReact
+				defaultColDef={DEFAULT_COLUMN_DEF}
+				rowData={rowData}
+				columnDefs={COLUMNS}
+				autoSizeStrategy={AUTO_SIZE_STRATEGY}
+				theme={THEME}
+			/>
 		</div>
 	);
 }
